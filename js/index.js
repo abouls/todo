1d991bd60042a7dad4767f011240588745dfea70
abouls <43948966@qq.com>
2017-05-19 20:48:54 +0800

    just to do~

---
+++ js/index.js
@@ -0,0 +1,43 @@
+/*
+This is an example from my Free Flexbox Starter Course. Check it out!
+https://unravelingflexbox.com/subscribe
+*/
+
+var count = 0;
+
+
+function sth(){
+var x = document.getElementById("todo")
+
+// alert(x.value);
+
+    
+
+
+    
+
+// document.getElementById("demo").innerHTML = x.value;
+
+
+ 
+ if (count>20){
+
+                alert("输入20条，不让输了！")
+
+                }else{
+
+                    var p= document.createElement('li');
+
+                    p.innerText=x.value;
+
+                    document.getElementById("point").appendChild(p);
+
+                    count++;
+
+
+                    alert("成功记录！")
+
+
+
+                        }   
+}
