02dc9a6cdd53b106e4a7853bb3a1129a602a7020
abouls <43948966@qq.com>
2017-05-20 23:23:03 +0800

    render react

--- js/index.js
+++ js/index.js
@@ -1,66 +1,35 @@
-var Mystorage = React.createClass({
-    handleClick: function () {
-        var y = document.getElementById("there").checked ? "完成" : "未完成";
-        var x = document.getElementById("here");
+class TodoInput extends React.Component {
+    onChange(event) {
+        console.log(event.target.value)
+    }
 
-        var p = document.createElement('li');
-        p.innerText = x.value + y;
-        document.getElementById("point").appendChild(p);
-    },
-    render () {
+    render() {
+        return (
+            <input type="text" onChange={this.onChange.bind(this)}/>
+        )
+    }
+}
+class TodoList extends React.Component {
+    render() {
         return (
             <div>
-                <input type="button" value="储存" onClick={this.handleClick}/>
+                <TodoInput/>
+                <ul>
+
+                    <li className="libg" id="pointb">记录表</li>
+                    <li className="libg" id="pointc">记录表</li>
+                    <li className="libg" id="pointd">记录表</li>
+                </ul>
             </div>
         );
     }
-});
-
-
-ReactDOM.render(
-    <Mystorage />,
-    document.getElementById('pointd')
-);
-
-
-var Content = React.createClass({
-    render () {
-        return (
-            <ul>
-                <li class="libg" id="point">请输入待办事项</li>
-                <li class="libg" id="pointb">记录表</li>
-                <li class="libg" id="pointc">记录表</li>
-                <li class="libg" id="pointd">记录表</li>
-            </ul>
-        );
-    }
-});
+}
+;
 
 ReactDOM.render(
-    <Content />,
+    <TodoList />,
     document.getElementById('list-content')
 );
-
-
-var ContentFT = React.createClass({
-    handleClick: function () {
-
-    },
-
-    render: function () {
-        return (
-            <div>
-                <input type="checkbox" id="there" ref="abox" onclick={this.handleClick}/>Did you finish this work?
-            </div>
-        );
-    }
-});
-
-ReactDOM.render(
-    <ContentFT />,
-    document.getElementById('pointc')
-);
-
 
 
 
