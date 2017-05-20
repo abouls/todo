b5348aadafcfdd4ec75c22f5d33f2f827e902240
abouls <43948966@qq.com>
2017-05-21 00:10:09 +0800

    5-20结束，页面内储存

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 class TodoInput extends React.Component {
     onChange(event) {
         console.log(event.target.value)
+        this.props.onChange(event.target.value)
     }
 
     render() {
@@ -10,15 +11,46 @@ class TodoInput extends React.Component {
     }
 }
 class TodoList extends React.Component {
+
+
+    constructor() {
+        super();
+        this.state = {todos: [], input: ""};
+    }
+
+    componentWillMount() {
+        this.setState({
+            todos: [
+                "react study", "dota2 ", "sleep", "sleep"
+            ]
+        })
+    }
+
+    changeText(text) {
+        this.setState({input: text})
+    }
+
+    save(){
+        var todo = this.state.input
+        const newTodos = this.state.todos
+        newTodos.push(todo)
+        this.setState({
+            todos :newTodos
+
+        })
+    }
+
+
     render() {
         return (
             <div>
-                <TodoInput/>
+                <TodoInput onChange={this.changeText.bind(this)}/>
+                <button onClick={this.save.bind(this)}>储存</button>
                 <ul>
+                    {this.state.todos.map(function (todo, index) {
+                        return <li className="libg" key={index}>{todo}</li>
+                    })}
 
-                    <li className="libg" id="pointb">记录表</li>
-                    <li className="libg" id="pointc">记录表</li>
-                    <li className="libg" id="pointd">记录表</li>
                 </ul>
             </div>
         );
